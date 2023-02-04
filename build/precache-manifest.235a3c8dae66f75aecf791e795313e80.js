7351909e132612501ed5814149d4addc0d90561e
rafang2206 <rafang22@hotmail.com>
2023-02-03 20:05:52 -0400

    update

---
+++ build/precache-manifest.235a3c8dae66f75aecf791e795313e80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d12348acc79f30bdd65ee1cfa73981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19abdf55bc282f17e521",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "19abdf55bc282f17e521",
+    "url": "/static/js/2.c0615879.chunk.js"
+  },
+  {
+    "revision": "093bf3f86145069d5ad0de50dd236547",
+    "url": "/static/js/2.c0615879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3d173158526f70b8f3",
+    "url": "/static/js/3.b93f18ee.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.b93f18ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10475ea6ae55e60ed52",
+    "url": "/static/js/main.cf2387c5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cf2387c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26727e7b167f7287ec97",
+    "url": "/static/js/runtime-main.dbe99236.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
